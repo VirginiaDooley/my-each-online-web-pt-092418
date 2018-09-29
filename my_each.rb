d2ac90b90216104a3a969021cde711233092fc27
Virginia Dooley <vcdooley@gmail.com>
2018-09-29 12:39:05 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,4 +1,7 @@
+i = 0 
+
 def my_each(collection)
+<<<<<<< HEAD
     i = 0
     while i < collection.length #iterates over collection array and yields after each element - and returns that element - while the length of the array is less than i
         yield(collection[i])
@@ -6,3 +9,10 @@ def my_each(collection)
     end
     collection
 end
+=======
+  while (i < collection.length) do |i| 
+  end
+  collection
+  end
+end
+>>>>>>> 0b8994e209184d478da14598a488ddbd95e553c5
